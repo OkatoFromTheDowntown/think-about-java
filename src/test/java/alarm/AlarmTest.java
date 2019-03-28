12dd9beb3c445e8c001d50030112543b9d8ee105
wengjiadong <316981067@qq.com>
2019-03-28 19:40:15 +0800

    test about superclass and subclass

--- src/test/java/alarm/AlarmTest.java
+++ src/test/java/alarm/AlarmTest.java
@@ -4,7 +4,7 @@ import main.java.alarm.Alarm;
 import main.java.alarm.WorldClock;
 
 public class AlarmTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassCastException {
 
         WorldClock clock = new WorldClock();
 
@@ -14,5 +14,61 @@ public class AlarmTest {
 
         clock.alarm();
 
+        AlarmTest test = new AlarmTest();
+
+        A a = test.new A();
+        B b = test.new B();
+        A c = test.new A();
+        B d = test.new B();
+        A e = test.new B();
+
+        a.name = "nameA";
+        a.age = "1";
+
+        b.setName("nameB");
+        b.setAge("2");
+
+        e.setName("nameE");
+        e.setAge("3");
+
+        c = b;
+
+//        d = (B) a; // ERROR!
+        d = (B) e;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
+
+    public class A {
+       private String name;
+       private String age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getAge() {
+            return age;
+        }
+
+        public void setAge(String age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return this.name + ", " + this.age;
+        }
+    }
+
+    public class B extends A {}
+
 }
+
